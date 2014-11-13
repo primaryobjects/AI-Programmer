634e19aa78d2227332f92bfde2d815618b1bb974
Kory Becker <kbecker@primaryobjects.com>
2014-11-12 22:09:25 -0500

    Optimized BottlesOfBeerFitness to work for any string.

--- AIProgrammer.Fitness/Concrete/BottlesOfBeerFitness.cs
+++ AIProgrammer.Fitness/Concrete/BottlesOfBeerFitness.cs
@@ -14,19 +14,32 @@ namespace AIProgrammer.Fitness.Concrete
     /// The user is prompted for input of a starting number. The program outputs from the starting number "x bottles of beer on the wall" until it reaches 0.
     /// Example: input = 5: 5 bottles of beer on the wall4 bottles of beer on the wall3 bottles of beer on the wall2 bottles of beer on the wall1 bottles of beer on the wall0 bottles of beer on the wall
     /// Note, the final output will be in byte format (including text, ie. 98 = 'b'). To display the output for humans, we have to convert the text byte values to char and leave the numbers as byte. This will correctly display 598498398 as 5b4b3b, since (char)98 = 'b'.
+    /// This class is best used with the BottlesOfBeerFunctions for appended code.
     /// </summary>
     public class BottlesOfBeerFitness : FitnessBase
     {
+        private static byte[][] _trainingExamples = { new byte[] { 5, 4, 3, 2, 1, 0 },
+                                      new byte[] { 3, 2, 1, 0 },
+                                      new byte[] { 2, 1, 0 }/*,
+                                      new byte[] { 6, 5, 4, 3, 2, 1, 0 }*/ };
+        private static string _targetString = "b";
+
+        /// <summary>
+        /// Previously generated BrainPlus functions for outputting the terms: bottles,of,beer,on,the,wall. This includes a 7th generated function that calls the other generated functions to output "bottles of beer". Generated using StrictStringFitness with StringFunction.
+        /// To use, set _appendCode = BottlesOfBeerFitness.BottlesOfBeerFunctions in main program.
+        /// </summary>
+        public static string BottlesOfBeerFunctions = "&6++.+++++++++++++[.+++[++..--------.-------.++++++++++++++.-%&[]7[[-.---------.[][.+>[t[-]-[,]+>,>++<]<]],>[]><]],-<]++,><%&6++.+++..7++.[6+[+<>[>>-]>><>-.+.><><-]<.<].+]..+-<+]+.[...,%&+8[+[+]7-.-.>[22+[[-..[[<-...][,.,<<>,>,,.,,]+<<++,]+>+,+]>+%&6+t+++i++[++-+[+++++[+++++++.-----[-[--[--[--.[-+----+.-[[[>%&7++++++[+.------[--------[--------.+++++++-+++++..[+>+>[++[<%&1+++++++++++++>a---<+++.>.---------.<.c[>>>[<,,>+<[-],+<>.<,%&d2.[e2.f]--->2[>-]<[>]v-......<+[+<>+>]].,+,>[<++,>><,]-.>,+%";
+
         public BottlesOfBeerFitness(GA ga, int maxIterationCount, string appendFunctions = null)
             : base(ga, maxIterationCount, appendFunctions)
         {
             if (_targetFitness == 0)
             {
-                // A * 28 * 256 => A = Number of numeric values in training example, 28 = number of characters in text, 256 = 256 characters per byte.
-                _targetFitness += 6 * 28 * 256;
-                _targetFitness += 4 * 28 * 256;
-                _targetFitness += 3 * 28 * 256;
-                _targetFitness += 7 * 28 * 256;
+                // Number of numeric values in training example * 256 + number of characters in target string * 256 * number of numeric values (since string is next to each number).
+                foreach (byte[] trainingExample in _trainingExamples)
+                {
+                    _targetFitness += (trainingExample.Length * 256) + (_targetString.Length * 256 * trainingExample.Length);
+                }
             }
         }
 
@@ -34,16 +47,10 @@ namespace AIProgrammer.Fitness.Concrete
 
         protected override double GetFitnessMethod(string program)
         {
-            byte[][] trainingExamples = { new byte[] { 5, 4, 3, 2, 1, 0 },
-                                      new byte[] { 3, 2, 1, 0 },
-                                      new byte[] { 2, 1, 0 },
-                                      new byte[] { 6, 5, 4, 3, 2, 1, 0 } };
-            string targetString = " bottles of beer on the wall";
-
             double countBonus = 0;
             double penalty = 0;
 
-            for (int i = 0; i < trainingExamples.Length; i++)
+            for (int i = 0; i < _trainingExamples.Length; i++)
             {
                 try
                 {
@@ -58,7 +65,7 @@ namespace AIProgrammer.Fitness.Concrete
                             state++;
 
                             // Send input.
-                            return trainingExamples[i][0];
+                            return _trainingExamples[i][0];
                         }
                         else
                         {
@@ -81,26 +88,30 @@ namespace AIProgrammer.Fitness.Concrete
                 _output.Append(_console.ToString());
                 _output.Append(",");
 
-                // Go through each sequence.
-                for (int j = 0; j < trainingExamples[i].Length; j++)
+                // Go through each sequence. 5bot4bot3bot2bot1bot0bot
+                for (int j = 0; j < _trainingExamples[i].Length; j++)
                 {
-                    int jj = j + j;
+                    int jj = j + (j * _targetString.Length);
 
-                    // Go through each item (5 bottles of beer on the wall). 5b4b3b2b
-                    for (int k = 0; k < 2; k++)
+                    // Go through each item (5 bottles of beer on the wall). + 1 for digit. and -1 to discard digit to index into text.
+                    for (int k = 0; k < _targetString.Length + 1; k++)
                     {
                         if (_console.Length > jj + k)
                         {
                             if (k == 0)
                             {
                                 // Verify digit.
-                                Fitness += 256 - Math.Abs((byte)_console[jj + k] - trainingExamples[i][j]);
+                                Fitness += 256 - Math.Abs((byte)_console[jj + k] - _trainingExamples[i][j]);
                             }
                             else
                             {
                                 // Verify text.
-                                Fitness += 256 - Math.Abs(_console[jj + k] - targetString[0]);
+                                Fitness += 256 - Math.Abs(_console[jj + k] - _targetString[k - 1]);
                             }
+                        }
+                        else
+                        {
+                            break;
                         }
                     }
                 }
