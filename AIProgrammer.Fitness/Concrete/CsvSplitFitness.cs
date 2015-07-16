3eb049e2cb7947461b95ce77acf39a0dca1b19a0
Kory Becker <kbecker@primaryobjects.com>
2015-07-16 16:03:15 -0400

    Added CsvSplitFitness.cs use with _genomeSize = 100.

---
+++ AIProgrammer.Fitness/Concrete/CsvSplitFitness.cs
@@ -0,0 +1,186 @@
+﻿using AIProgrammer.Fitness.Base;
+using AIProgrammer.GeneticAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIProgrammer.Fitness.Concrete
+{
+    /// <summary>
+    /// Splits a string into csv format by splitting it every x characters and separating by a comma.
+    /// Example:
+    /// Input: Hello World
+    /// Output: Hell,o W,orl,d
+    /// </summary>
+    public class CsvSplitFitness : FitnessBase
+    {
+        private static string[] _trainingExamples = { "hello", "orange", "spaghetti", "wins", "one" };
+        private static string[] _trainingResults = new string[_trainingExamples.Length];
+        private int _length;
+
+        public CsvSplitFitness(GA ga, int maxIterationCount, int length, string appendFunctions = null)
+            : base(ga, maxIterationCount, appendFunctions)
+        {
+            _length = length;
+
+            if (_targetFitness == 0)
+            {
+                for (int i = 0; i < _trainingExamples.Length; i++)
+                {
+                    string csvString = string.Join(",", SplitInParts(_trainingExamples[i], _length));
+
+                    _trainingResults[i] = csvString;
+                    _targetFitness += csvString.Length * 256;
+                }
+            }
+        }
+
+        #region FitnessBase Members
+
+        protected override double GetFitnessMethod(string program)
+        {
+            double countBonus = 0;
+            double penalty = 0;
+
+            for (int i = 0; i < _trainingExamples.Length; i++)
+            {
+                try
+                {
+                    int state = 0;
+                    _console.Clear();
+
+                    // Run the program.
+                    _bf = new Interpreter(program, () =>
+                    {
+                        if (state < _trainingExamples[i].Length)
+                        {
+                            // Send input.
+                            return (byte)_trainingExamples[i][state++];
+                        }
+                        else
+                        {
+                            // Not ready for input.
+                            return 0;
+                        }
+                    },
+                    (b) =>
+                    {
+                        _console.Append((char)b);
+                    });
+                    _bf.Run(_maxIterationCount);
+                }
+                catch
+                {
+                }
+
+                _output.Append(_console.ToString());
+                _output.Append("|");
+
+                // Check result.
+                for (int j = 0; j < _trainingResults[i].Length; j++)
+                {
+                    if (_console.Length > j)
+                    {
+                        Fitness += 256 - Math.Abs(_console[j] - _trainingResults[i][j]);
+                    }
+                }
+
+                // Length bonus (percentage of 100).
+                countBonus += 10 * ((_trainingResults[i].Length - Math.Abs(_console.Length - _trainingResults[i].Length)) / _trainingResults[i].Length);
+
+                // Make the AI wait until a solution is found without the penalty (too many input characters).
+                Fitness -= penalty;
+
+                // Check for solution.
+                IsFitnessAchieved();
+
+                // Bonus for less operations to optimize the code.
+                countBonus += ((_maxIterationCount - _bf.m_Ticks) / 20.0);
+
+                Ticks += _bf.m_Ticks;
+            }
+
+            if (_fitness != Double.MaxValue)
+            {
+                _fitness = Fitness + countBonus;
+            }
+
+            return _fitness;
+        }
+
+        protected override void RunProgramMethod(string program)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                // Get input from the user.
+                Console.WriteLine();
+                Console.Write(">: ");
+                string line = Console.ReadLine();
+                int index = 0;
+
+                _console.Clear();
+
+                try
+                {
+                    // Run the program.
+                    Interpreter bf = new Interpreter(program, () =>
+                    {
+                        byte b;
+
+                        // Send the next character.
+                        if (index < line.Length)
+                        {
+                            b = (byte)line[index++];
+                        }
+                        else
+                        {
+                            b = 0;
+                        }
+
+                        return b;
+                    },
+                    (b) =>
+                    {
+                        _console.Append((char)b);
+                    });
+
+                    bf.Run(_maxIterationCount);
+                }
+                catch
+                {
+                }
+
+                Console.WriteLine(_console.ToString());
+            }
+        }
+
+        public override string GetConstructorParameters()
+        {
+            return _maxIterationCount + ", " + _length;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// http://stackoverflow.com/a/4133475/2596404
+        /// </summary>
+        private IEnumerable<String> SplitInParts(string s, int partLength)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("s");
+            }
+            if (partLength <= 0)
+            {
+                throw new ArgumentException("Part length has to be positive.", "partLength");
+            }
+
+            for (var i = 0; i < s.Length; i += partLength)
+            {
+                yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+            }
+        }
+    }
+}
